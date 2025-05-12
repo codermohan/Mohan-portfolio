8bd8132ec366e9eb04652678c9fd459fc001c035
Mohan Venkata Krishna Namburi <mohankrishnan802@gmail.com>
2025-05-12 11:35:20 -0500

    Upgrades

--- utils/data/testimonials.js
+++ utils/data/testimonials.js
@@ -1,7 +1,7 @@
 export const testimonials = [
     {
       id: 1,
-      name: "Carolyn Seaman",
+      name: "Nikhil Chakravarthy Mallela",
       position: "Professor and Director of CWIT, UMBC",
       message: "Neha is sharp, organized, and highly personable. Her completed Masters in Data Science has provided a number of relevant and useful skills in data management and analysis. While at CWIT, she also became proficient in several reporting tools that she used to help us make sense of our data. I highly recommend her for data science-related positions in any domain.",
       date: "Nov 19, 2024",
@@ -9,7 +9,7 @@ export const testimonials = [
     },
     {
         id: 2,
-        name: "Tony Diana, Ph.D.",
+        name: "Krishna Sai Avinash Potturi",
         position: "Division Manager, Outreach, Office of NextGen | Adjunct Lecturer at UMBC",
         message: "Neha is an exceptional student. She is continuously striving to learn new methods and algorithms and to stay competitive. She has several qualities that make her qualified for a data scientist position and responsibilities as a leader in an organization: excellent technical and programming skills, listening and execution capability, and a disposition to work well in a teamwork environment. I am delighted to recommend Neha for a data scientist position. She will be able to be operational quickly.",
         date: "Sept 13, 2023",
@@ -17,7 +17,7 @@ export const testimonials = [
     },
     {
         id: 3,
-        name: "Tristan King",
+        name: "Venkata Sai Mohan Yedla",
         position: "Research Software Engineer at Imaging Research Center, UMBC",
         message: "Neha is incredibly hard working, diligent and insightful. She was a joy to work with on various data science research tasks at the Imaging Research Center, and was always exceeding expectations. I would welcome the opportunity to work with her again. If you have the chance to work with Neha, take it!",
         date: "Sept 13, 2023",
