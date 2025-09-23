ef3834fbd8595ed7817251ca8c20a1a38dadfddb
Mohan Venkata Krishna Namburi <mohankrishnan802@gmail.com>
2025-09-23 09:19:56 -0500

    Add

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Mohan Venkata Krishna Namburi",
   profile: '/profile.jpg',
   designation: "Devops Engineer | Building scalable and efficient systems",
-  description: "Hello! I’m Mohan Venkata Krishna Namburi, a DevOps Engineer who enjoys solving problems and building things that work well and help people. I’m always curious and love learning something new every day — whether it’s a new tool, a better way to write code, or how to fix something that broke five minutes ago.",
+  description: "Hello! I’m Mohan Venkata Krishna Namburi, a DevOps En who enjoys solving problems and building things that work well and help people. I’m always curious and love learning something new every day — whether it’s a new tool, a better way to write code, or how to fix something that broke five minutes ago.",
   description2: "I care about writing clean code, working as a team, and improving step by step. I’ve worked on websites, automation tools, and cloud-based projects, and I enjoy seeing my work come to life. I’m looking for new opportunities where I can learn more, take on challenges, and contribute to projects that make a real impact.",
   email: "mohankrishnan802@gmail.com",
   phone: "+12487989463",
@@ -14,7 +14,7 @@ export const personalData = {
   stackOverflow: '',
   leetcode: "",
   devUsername: "",
- // resume: "https://docs.google.com/document/d/1gmbbe-iz8ZCs4a5LCUiJ3ZO7ONNq7i-d/edit?usp=drivesdk&ouid=102633953369002209964&rtpof=true&sd=true"
+  resume: ""
   
 }
 
